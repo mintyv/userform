4bb15d66c09e5e3fa87ea1e27ed7c8843c17fe51
mintyv <94420937+mintyv@users.noreply.github.com>
2021-12-04 20:22:56 -0600

    React Table
    Functions are non-functioning.

---
+++ Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Junk in the trunk</p>
+        <ol>
+          <li>Free</li>
+          <li>Donate</li>
+          <li>Toss</li>
+          <li>Recycle</li>
+          <li>Charity</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
