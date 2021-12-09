00d02266435cb9d91ac965197bc3966bf944b725
mintyv <vangm29@gmail.com>
2021-12-09 09:54:15 -0600

    change

---
+++ login-system/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+
+
+const App = ()=> {
+  return (
+    <div className="App">
+     <h1>Mina's React Login</h1>
+    </div>
+  );
+};
+
+export default App;
