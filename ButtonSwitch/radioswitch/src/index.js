dd5c308aa5e5ba4b0d47a6aca6d8476f7b820d03
mintyv <vangm29@gmail.com>
2021-12-13 11:25:21 -0600

    change

--- ButtonSwitch/radioswitch/src/index.js
+++ ButtonSwitch/radioswitch/src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import allReducers from './reducers';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { logger } from 'redux-logger';
 
-const store = createStore(allReducers);
+const myMiddleware = store => next =>{
+  // console.log("Middleware ran:");
+
+  return next(action);
+}
+
+const capAtTen = store => next => action =>{
+  if(store.getState().counter >= 10){
+    return next({type : "DECREMENT"});
+  }
+  return next(action);
+  }
+
+
+// const MyMiddleware = store =>{
+//   return next => {
+//     return action =>{
+//       console.log("Middleware ran");
+//       return next(action);
+//     }
+//   }
+// }
+const store = createStore(allReducers, composeWithDevTools(applyMiddleware(myMiddleware, secondMiddleware, capAtTen, logger)));
 
 ReactDOM.render(
   <Provider store = {store}>
