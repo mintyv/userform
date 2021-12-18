7ee223f2a050b76991c13822af3308172a949881
mintyv <vangm29@gmail.com>
2021-12-18 16:43:46 -0600

    changes

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import postReducer from './reducers';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+const store = createStore(postReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
